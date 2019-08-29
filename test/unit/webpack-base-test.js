ca782be1bff50182bea420506bb791a6c6dd08ac
ss7m <Sean.Y.Shu@newegg.com>
2019-08-29 11:41:11 +0800

    test: fix bug in webpack base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ describe("webpack base test", () => {
   const baseConf = require("../../lib/webpack.base");
   it("entry", () => {
     assert.equal(
-      baseConf.entry.index.indexof("/test/smoke/template/src/index/main.tsx") >
+      baseConf.entry.index.indexOf("/test/smoke/template/src/index/main.tsx") >
         -1,
       true
     );
